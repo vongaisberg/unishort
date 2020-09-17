dce5916156790671966f9fe6b0dca15b28475914
Maximilian von Gaisberg <github@vongaisberg.de>
2020-09-17 10:53:46 +0200

    Add indexes to db

--- migrations/2020-08-24-225814_create_urls/down.sql
+++ migrations/2020-08-24-225814_create_urls/down.sql
@@ -1,1 +1,4 @@
-DROP TABLE urls-- This file should undo anything in `up.sql`
+DROP INDEX short_url_index;
+DROP INDEX url_index;
+
+DROP TABLE urls;-- This file should undo anything in `up.sql`
--- migrations/2020-08-24-225814_create_urls/up.sql
+++ migrations/2020-08-24-225814_create_urls/up.sql
@@ -3,4 +3,6 @@ CREATE TABLE urls (
     url VARCHAR UNIQUE NOT NULL,
     short_url VARCHAR UNIQUE NOT NULL,
     timestamp TIMESTAMP NOT NULL
-  )
+  );
+CREATE INDEX short_url_index ON urls (short_url);
+CREATE INDEX url_index ON urls (url);
